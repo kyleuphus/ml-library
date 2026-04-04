076b3e1d9b5460bde7c44e98cc3d0ed21f1c27ce
kyleuphus <kyleuphus@gmail.com>
2026-04-04 02:00:20 -0400

    implemented tanh activation function

--- gradient-descent/value_class.py
+++ gradient-descent/value_class.py
@@ -8,22 +8,55 @@ class Value:
 
     def __add__(self, other):
         addition = self.data + other.data
-        return Value(addition, (self, other), "+")
+        out = Value(addition, (self, other), "+")
 
         def _backward():
-            
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
 
+        out._backward = _backward
+
+        return out
 
     def __mul__(self, other):
         multiplication = self.data * other.data
-        return Value(multiplication, (self, other), "*")
+        out = Value(multiplication, (self, other), "*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad = self.data * out.grad
+
+        out._backward = _backward
+
+        return out
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
+    # depth first search for topoligical list
+    def backwards(self):
+        topo = []
+        visited = set()
 
-a = Value(8)
-b = Value(12)
+        def _back(v):
+            if v not in visited:
+                visited.add(v)
+
+                for child in v._prev:
+                    _back(child)
+                topo.append(v)
+
+        _back(self)
+        self.grad = 1.0
+
+        for node in reversed(topo):
+            node._backward()
+
+
+a = Value(2.0)
+b = Value(-4.0)
 c = a * b
-c += b
-print(c._prev) 
+d = c + a
+
+d.backwards()
+print(f"a: {a.grad}, b: {b.grad}, c: {c.grad}")
