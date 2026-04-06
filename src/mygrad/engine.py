f2f1269c793d4657fa42d6c4e996859910eabba9
kyleuphus <kyleuphus@gmail.com>
2026-04-05 23:09:43 -0400

    implemented relu activation function

--- src/mygrad/engine.py
+++ src/mygrad/engine.py
@@ -2,11 +2,11 @@ import math
 
 
 class Value:
-    def __init__(self, data, _children=(), _ops=""):
+    def __init__(self, data, _children=(), _op=""):
         self.data = data
         self.grad = 0.0
         self._prev = set(_children)
-        self._op = _ops
+        self._op = _op
         self._backward = lambda: None
 
     def __add__(self, other):
@@ -88,6 +88,15 @@ class Value:
 
         def _backward():
             self.grad += (1.0 - t**2) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), "relu")
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
 
         out._backward = _backward
         return out
