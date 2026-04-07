b90f29997fc80defb92290444a0eedcd395d82a3
kyleuphus <kyleuphus@gmail.com>
2026-04-07 00:33:13 -0400

    implement layer & mlp clases

--- src/mygrad/nn/layers.py
+++ src/mygrad/nn/layers.py
@@ -18,3 +18,23 @@ class Neuron:
             return activation.relu()
         elif self.act in [None, "linear"]:
             return activation
+
+
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in nout]
+
+    def __call__(self, x):
+        outputs = [n(x) for n in self.neurons]
+        return outputs
+
+
+class MLP:
+    def __init__(self, nin, nouts):
+        size = [nin] + nouts
+        self.layers = [Layer(size[i], size[i + 1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
