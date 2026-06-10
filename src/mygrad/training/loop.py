8d7199503eac4ea5284eb94159b287527f6cc276
kyleuphus <kyleuphus@gmail.com>
2026-06-09 20:02:55 -0400

    implement learning rate decay and loss arg to training function

--- src/mygrad/training/loop.py
+++ src/mygrad/training/loop.py
@@ -6,23 +6,29 @@
 # for param in params, param += lr * (-param.grad) (sgd)
 # forward pass and repeat from there
 import numpy as np
-
+import math
 from mygrad.nn.mlp import Tensor, MLP
 from mygrad.optim.sgd import SGD
 
 
 def train(
-    mlp: MLP, input: Tensor, target: Tensor, loss: str = "mse", epochs: int = 1000
+    mlp: MLP,
+    input: Tensor,
+    target: Tensor,
+    loss: callable,
+    lr: float = 0.0001,
+    lr_decay: float = 1.0,
+    epochs: int = 1000,
 ):
-    grad = SGD(mlp.parameters())
-    i = 0
+    grad = SGD(mlp.parameters(), lr=lr)
+    initial_lr = lr
     for epoch in range(epochs):
         out = mlp(input)
-        mse = ((out - target) * (out - target)).sum() / float(out.data.size)
-        if i in [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]:
-            print(mse.data)
+        _loss = loss(out, target)
+        grad.lr = initial_lr * math.pow(lr_decay, epoch)
+        if (epoch + 1) % 500 == 0:
+            print(f"epoch: {epoch + 1} : {_loss.data}")
         mlp.zero_grad()
-        mse.grad = np.ones_like(mse.data)
-        mse.backward()
+        _loss.grad = np.ones_like(_loss.data)
+        _loss.backward()
         grad.step()
-        i += 1
